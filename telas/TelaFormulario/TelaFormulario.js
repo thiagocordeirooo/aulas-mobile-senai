023c4bda4ddf541f7be64737b51577564cd9754b
Thiago Cordeiro <thiago.cordeiro@mercadolivre.com>
2024-04-25 20:04:55 -0300

    Inputs Switch, RNPickerSelect

--- telas/TelaFormulario/TelaFormulario.js
+++ telas/TelaFormulario/TelaFormulario.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Switch, View } from 'react-native';
+import RNPickerSelect from 'react-native-picker-select';
 import BotaoCustomizado from '../../comum/componentes/BotaoCustomizado/BotaoCustomizado';
 import CampoTextoCustomizado from '../../comum/componentes/CampoTextoCustomizado/CampoTextoCustomizado';
 import CORES from '../../comum/constantes/cores';
-import estilos from './TelaFormularioStyle';
+
+import { estilos, pickerSelectStyles } from './TelaFormularioStyle';
 
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
@@ -11,9 +13,11 @@ const TelaFormulario = () => {
   const [campoNome, setCampoNome] = React.useState('');
   const [campoSobrenome, setCampoSobrenome] = React.useState('');
   const [campoCPF, setcampoCPF] = React.useState('');
+  const [campoAceitaTermos, setCampoAceitaTermos] = React.useState(false);
+  const [campoEsporte, setCampoEsporte] = React.useState('');
 
   const salvar = () => {
-    console.log('Salvar:', { campoNome, campoSobrenome, campoCPF });
+    console.log('Salvar:', { campoNome, campoSobrenome, campoCPF, campoAceitaTermos, campoEsporte });
   };
 
   return (
@@ -25,6 +29,28 @@ const TelaFormulario = () => {
       <CampoTextoCustomizado label='Nome' value={campoNome} onChangeText={setCampoNome} />
       <CampoTextoCustomizado label='Sobrenome' value={campoSobrenome} onChangeText={setCampoSobrenome} />
       <CampoTextoCustomizado label='CPF' inputMode='numeric' value={campoCPF} onChangeText={setcampoCPF} />
+
+      <Switch
+        trackColor={{ false: '#767577', true: '#81b0ff' }}
+        thumbColor={campoAceitaTermos ? '#f5dd4b' : '#f4f3f4'}
+        onValueChange={setCampoAceitaTermos}
+        value={campoAceitaTermos}
+      />
+
+      <RNPickerSelect
+        style={pickerSelectStyles}
+        onValueChange={setCampoEsporte}
+        value={campoEsporte}
+        items={[
+          { label: 'Futebol', value: '1' },
+          { label: 'Baseball', value: '2' },
+          { label: 'Volei', value: '3' },
+          { label: 'Surf', value: '4' },
+          { label: 'Sakate', value: '5' },
+        ]}
+        placeholder={{ label: 'Selecione um Esporte', value: null }}
+      />
+
       <BotaoCustomizado onPress={salvar}>Salvar</BotaoCustomizado>
     </View>
   );
--- telas/TelaFormulario/TelaFormularioStyle.js
+++ telas/TelaFormulario/TelaFormularioStyle.js
@@ -1,6 +1,6 @@
 import { StyleSheet } from 'react-native';
 
-const estilos = StyleSheet.create({
+export const estilos = StyleSheet.create({
   container: {
     flex: 1,
     padding: 16,
@@ -8,4 +8,25 @@ const estilos = StyleSheet.create({
   },
 });
 
-export default estilos;
+export const pickerSelectStyles = StyleSheet.create({
+  inputIOS: {
+    fontSize: 16,
+    paddingVertical: 12,
+    paddingHorizontal: 10,
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 4,
+    color: 'black',
+    paddingRight: 30, // to ensure the text is never behind the icon
+  },
+  inputAndroid: {
+    fontSize: 16,
+    paddingHorizontal: 10,
+    paddingVertical: 8,
+    borderWidth: 0.5,
+    borderColor: 'purple',
+    borderRadius: 8,
+    color: 'black',
+    paddingRight: 30, // to ensure the text is never behind the icon
+  },
+});
